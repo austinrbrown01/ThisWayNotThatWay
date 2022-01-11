b2cec5ff2ac66e033bc4b84b810de2c967f83c0a
Austin <austinrbrown01@gmail.com>
2022-01-11 16:26:32 -0700

    proper collapsing

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,18 +1,20 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 
 const Navigation = () => {
   return (
     <>
       <Navbar collapseOnSelect fixed='top' expand="sm" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="/" bold >This way, not that way!</Navbar.Brand>
+          <Navbar.Brand href="/">This way, not that way!</Navbar.Brand>
           <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-          <Navbar.Collapse id='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
           <Nav>
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/">About</Nav.Link>
-            <Nav.Link href="/">Support</Nav.Link>
+            <Nav.Link href="/" hidden-xs>About</Nav.Link>
+            <Nav.Link href="/" hidden-xs>Support</Nav.Link>
           </Nav>
+          </Navbar.Collapse>
         </Container>
 
       </Navbar>
